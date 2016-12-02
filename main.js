0eebe0a9de86ad4a4762e9d2ef3e58cea43fa733
Pavel Petrenko <palkan1701@mail.ru>
2016-12-02 10:44:07 +0200

    new changes

--- main.js
+++ main.js
@@ -1,33 +1,65 @@
 
 var quantity, exchRate, calculatedValue;
-var defaultQuantity = 100;
-var defultExchRate = 26;
+var defaultQuantity = "1";
+var defultExchRate = "8";
+var money = document.getElementById("quantity");
+var rate = document.getElementById("exchRate");
+var sum = document.getElementById("add-sum");
 
-function getGuantity() {
-	while (quantity === undefined || isNaN(quantity)) {
-		quantity = +prompt('Enter value to exchange (only numbers)', defaultQuantity);
+
+
+
+var inputText = function (e) {
+	if (typeof money !== 'number') {
+		e.preventDefault();
 	}
-};
+	
+}
 
-function getExchRate() {
-	while (exchRate === undefined || isNaN(exchRate)) {
-		exchRate = +prompt('Enter exchange rate', defultExchRate);
+
+money.addEventListener("input", getQuantity);
+rate.addEventListener("input", getQuantity);
+	
+	function getQuantity () {
+	var one = parseFloat(money.value) || 0;
+	var two = parseFloat(rate.value) || 0;
+	calculatedValue =(one/two);
+	if (calculatedValue == NaN) {
+		sum.innerHTML = "";
 	}
-};
-getGuantity();
-getExchRate();
-
-if (exchRate < 25 ){
-		alert('Marty, we have to go back!');
-var exchRate = "line"    
-getExchRate();
-console.log(getExchRate)
-
+	else if (one<two){
+		sum.innerHTML = "";
+	
+	} 
+	else if (calculatedValue == "Infinity") {
+		sum.innerHTML = "";
+	}
+	else {
+	sum.innerHTML = "Your will got: " +calculatedValue.toFixed(2) +" $ ";
+	}
 }
-else {
-alert('Looks like actual data');
-}
-calculatedValue = Math.round(quantity/exchRate);
 
-alert('You will got ~ $' + calculatedValue);
+
+
+// function getExchRate() {
+// 	while (exchRate === undefined || isNaN(exchRate)) {
+// 		exchRate = +prompt('Enter exchange rate', defultExchRate);
+// 	}
+// };
+// getGuantity();
+// getExchRate();
+
+// if (exchRate < 25 ){
+// 		alert('Marty, we have to go back!');
+// var exchRate = "line"    
+// getExchRate();
+// console.log(getExchRate)
+
+// }
+// else {
+// alert('Looks like actual data');
+// }
+// calculatedValue =((quantity/exchRate).toFixed(2));
+
+// alert('You will got ~ $' + calculatedValue);
 
